bcf6e4220d9fcfb5989df754a51fd1412d9cbc50
Diego <diegosilva@aluno.saojudas.br>
2022-04-18 23:21:54 -0300

    codigo para identificar nomes da cidade na  tabela

---
+++ verificar_cidades_csv_ocorrencias.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import csv
+
+file = pd.read_csv("csv\ds_SSP_ocorrencias_mensais_2001-2019.csv",encoding="utf-8")
+#arquivo recebe uma lista do file somente da coluna cidade
+arquivo = csv.reader(file["Cidade"])
+
+valor= 0
+nomes = []
+#for pecorre toda a lista do arquivo
+for i, linha in enumerate(arquivo):
+    #if exibe o conteudo da linha somente quando i for maior que valor, 
+    # valor e referencia para buscar nomes da cidades pulando todos os anos em que ela aparece
+    if i >= valor :
+     print("leitura: ", i,linha)
+     #nomes insere o conteudo de linha, para armazenar os nomes da cidades que existe na tabela
+     nomes.insert(i, linha)
+     #valor e somado + 414, pois para cada nova cidade aparecer, e preciso pular 414 linhas
+     valor = valor+414
+     #apos a leitura de 276673 linhas contidas na tabela o loop termina
+    if valor > 276673 :
+        break
+
+for linhas in nomes:
+    print(linhas)  
+
+#aqui exibe o tamanho da lista nomes para identificar a quantidade de cidade que existe na tabela
+print("tamanho nomes: ", len(nomes))    
